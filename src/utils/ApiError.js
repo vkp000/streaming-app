6e0125bc63395e60d418ef7d456930cfbb382687
vivek <vivekprajapatvkp0@gmail.com>
2025-06-03 11:41:00 +0530

    Setup for the project is done

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,17 +2,17 @@ class ApiError extends Error {
     constructor(
         statusCode,
         message="Something went wrong",
-        error=[],
-        statck = ""
+        errors=[],
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
         this.data = null
         this.message = message 
         this.success = false;
-        this.errors = this.errors
-        if(statck){
-            this.stack = statck
+        this.errors = errors
+        if(stack){
+            this.stack = stack
         }else{
             Error.captureStackTrace(this, this.constructor)
         }
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET  
+});
+
+const uploadOnCloudinary = async (localFilepath) => {
+    try {
+        if(!localFilepath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilepath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilepath) // remove the locally saved temporary file as the upload operation got failed 
+    }
+}
+
+
+export {uploadOnCloudinary}
